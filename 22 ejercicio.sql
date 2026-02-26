eb67770978b39d2d367b407e7e16bd046e802346
Andresb275 <habuesaquillo.0730@unicesmag.edu.co>
2026-02-26 00:20:43 -0500

    solucion ejer_22

---
+++ 22 ejercicio.sql
@@ -0,0 +1,19 @@
+/*
+EJERCICIO 22
+Objetivo: Clasificar clientes según nivel de gasto.
+
+Explicación:
+Se suma el gasto total por cliente.
+Se usa CASE WHEN para clasificar.
+*/
+
+SELECT c.nombre,
+       SUM(v.total_venta) AS total_gastado,
+       CASE
+           WHEN SUM(v.total_venta) > 5000 THEN 'Alto Valor'
+           WHEN SUM(v.total_venta) BETWEEN 2000 AND 5000 THEN 'Medio Valor'
+           ELSE 'Bajo Valor'
+       END AS categoria_gasto
+FROM ventas v
+JOIN clientes c ON v.id_cliente = c.id_cliente
+GROUP BY v.id_cliente;
