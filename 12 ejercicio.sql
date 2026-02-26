89e2b81a7e5db10310b3496b6439a367ed74cbc7
Andresb275 <habuesaquillo.0730@unicesmag.edu.co>
2026-02-25 23:57:14 -0500

    solucion ejer_12

---
+++ 12 ejercicio.sql
@@ -0,0 +1,11 @@
+/*
+EJERCICIO 12
+Objetivo: Precio promedio de los productos.
+
+Explicación:
+AVG(precio) calcula el valor promedio
+de todos los productos registrados.
+*/
+
+SELECT AVG(precio) AS precio_promedio
+FROM productos;
